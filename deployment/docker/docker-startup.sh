53fac50e68c117b204d85c1c6b0d3e2562c502e6
daniel.perebinos@ebs-integrator.com <daniel.perebinos@ebs-integrator.com>
2025-05-30 18:47:31 +0300

    Fixing liveness

--- deployment/docker/docker-startup.sh
+++ deployment/docker/docker-startup.sh
@@ -5,4 +5,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Start Uvicorn"
-exec uvicorn config.asgi:application --host 0.0.0.0 --port 8000 --reload
+exec uvicorn config.asgi:application --host 0.0.0.0 --port 8000
